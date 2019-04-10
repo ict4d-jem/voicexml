243a28551c0608f45adc24c53afc5ef1898bc67c
Ersin <ersinpw@gmail.com>
2019-04-10 18:14:40 +0200

    Update log.php

--- debug/log.php
+++ debug/log.php
@@ -5,5 +5,5 @@
  */
 
 $h = fopen("./access.log", "w") or die("Unable to open file!");
-fwrite($h, html_entity_decode($_GET['log'], ENT_COMPAT, 'UTF-8'));
+fwrite($h, html_entity_decode(print_r($_GET, true), ENT_COMPAT, 'UTF-8'));
 fclose($h);
