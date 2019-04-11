e5d433dcd59de0efcfc53c2695a5f00266a5c995
mgamanunes <mgamanunes@gmail.com>
2019-04-11 16:07:14 +0200

    small detail

--- identity/main.php
+++ identity/main.php
@@ -2,12 +2,12 @@
 
 include_once("identity_utils.php");
 
-if(empty($_REQUEST['session_callerid'])) {
+if(empty($_REQUEST['session.callerid'])) {
     include 'error.vxml';
     die();
 }
 else {
-    $user_data = get_user($_GET['session_callerid']);
+    $user_data = get_user($_GET['session.callerid']);
     if(!$user_data)
         include "identification.php";
     else
