db04aeb03096f01e40792f449635685b802c2ea6
mgamanunes <mgamanunes@gmail.com>
2019-04-11 17:25:09 +0200

    Added more details to the identification

--- identity/save_record.php
+++ identity/save_record.php
@@ -1,32 +1,47 @@
 <?php
 
+$regions = array(
+    array(
+        "name" => "Noord-Holland",
+        "villages" => array("Amsterdam", "Aalkmar")
+    ),
+    array(
+        "name" => "Drente",
+        "villages" => array("Assen", "Oranje")
+    ),
+    array(
+        "name" => "Flevoland",
+        "villages" => array("Lelystad", "Almere")
+    )
+);
+
 $filename = $_REQUEST['filename'];
 
 if(ctype_digit($filename))
 {
 
-  $file  = $_FILES['identity']['tmp_name'];
-  $ctype = $_FILES['identity']['type'];
+    $file  = $_FILES['identity']['tmp_name'];
+    $ctype = $_FILES['identity']['type'];
 
-  switch ($ctype)
-  {
-    case "video/3gpp":
-    case "video/3gp":
-       $ext = ".3gp";
-       break;
-    case "image/tiff":
-       $ext = ".tiff";
-       break;
-    default:
-       $ext = ".wav";
-       break;
-  }
+    switch ($ctype)
+    {
+        case "video/3gpp":
+        case "video/3gp":
+            $ext = ".3gp";
+            break;
+        case "image/tiff":
+            $ext = ".tiff";
+            break;
+        default:
+            $ext = ".wav";
+            break;
+    }
 
-  if(file_exists("records/".$filename))
-    unlink("records/".$filename);
+    if(file_exists("records/".$filename))
+        unlink("records/".$filename);
 
-  if(move_uploaded_file($file, "records/".$filename.$ext))
-    print "";
+    if(move_uploaded_file($file, "records/".$filename.$ext))
+        print "";
 }
 ?><?xml version="1.0" encoding="UTF-8"?>
 <vxml version="2.1">
