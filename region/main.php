ef8638a77fdaa4923c673efc311ed96dcdbcab3a
Ersin <ersinpw@gmail.com>
2019-04-12 18:27:52 +0200

    Update main.php

--- region/main.php
+++ region/main.php
@@ -8,5 +8,5 @@ echo $twig->render('selection.xml.twig', [
     'id'        => 'regions',
     'nextid'    => 'index.php?id=villages',
     'choices'   => array_keys($json_decode['region']),
-    'message'   => '<break time="1000"/>Thank you! We will now ask you a few questions so we can build a profile on you. We will not request this information again.<break time="50"/>Please select your region from the following options</prompt>'
+    'message'   => '<break time="1000"/>Thank you! We will now ask you a few questions so we can build a profile on you. We will not request this information again.<break time="50"/>Please select your region from the following options'
 ]);
