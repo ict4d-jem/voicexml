0bd39eb1a7a2748bd84daa692365398a52f05ae8
Ersin <ersinpw@gmail.com>
2019-04-12 21:47:13 +0200

    Update main.php

--- thank_you/main.php
+++ thank_you/main.php
@@ -2,5 +2,5 @@
 echo $twig->render('message.xml.twig', [
     'id'        => 'thank_you',
     'message'   => '<break time="1000"/>Thank you',
-    'footer'    => '<block><submit next="identity/save_record.php" namelist="recorded_record_name filename language latest_crop regions vilages cropsize" method="post" enctype="multipart/form-data" fetchtimeout="10s"/></block>'
+    'footer'    => '<block><submit next="identity/save_record.php" namelist="recorded_record_name filename language latest_crop regions villages cropsize" method="post" enctype="multipart/form-data" fetchtimeout="10s"/></block>'
 ]);
