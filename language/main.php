4c2acbc9683cdd1cf41f75b4ae11f2d2df4b1bb0
Ersin <ersinpw@gmail.com>
2019-04-12 20:30:43 +0200

    Update main.php

--- language/main.php
+++ language/main.php
@@ -2,7 +2,7 @@
 $language = file_get_contents("json/language.json");
 $json_decode = json_decode($language, true);
 echo $twig->render('selection.xml.twig', [
-    'id'        => 'cropsize',
+    'id'        => 'language',
     'nextid'    => 'index.php?id=welcome&session_callerid='.$_GET['session_callerid'],
     'choices'   => $json_decode,
     'message'   => '<break time="1000"/>Please select your language<break time="50"/>'
