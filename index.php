c8f37cc4ac4e4d97cbb5a18b901cf81c92dcf8bd
Ersin <ersinpw@gmail.com>
2019-04-12 18:15:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 require_once __DIR__.'/bootstrap.php';
 
-include "identity/main.php";
+if(
+  isset($_GET) &&
+  isset($_GET['id']) &&
+  in_array($_GET['id'], array(
+    'identity',
+    'region',
+    'village',
+    'crop_size'
+  )
+)
+  include $_GET['id'].'/main.php';
+else
+  include "identity/main.php";
