510a038baa2c5896a8483ed676804dc75ff28bfd
Ersin <ersinpw@gmail.com>
2019-04-12 19:33:25 +0200

    Update main.php

--- villages/main.php
+++ villages/main.php
@@ -5,6 +5,6 @@ $json_decode = json_decode($region, true);
 echo $twig->render('selection.xml.twig', [
     'id'        => 'villages',
     'nextid'    => 'index.php?id=crop_size&session_callerid='.$_GET['session_callerid'],
-    'choices'   => array_keys($json_decode['region'][$_GET['selected']]),
+    'choices'   => array_keys($json_decode['region'][$_GET['selected']]['cercle']),
     'message'   => '<break time="1000"/>Choose your village'
 ]);
