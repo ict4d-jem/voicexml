5959fcb8f7a25e0176eaca2bb53ee005e5f1cca6
Ersin <ersinpw@gmail.com>
2019-04-12 17:18:33 +0200

    Update main.php

--- identity/main.php
+++ identity/main.php
@@ -3,13 +3,19 @@
 include_once("identity_utils.php");
 
 if(empty($_REQUEST['session_callerid'])) {
-    echo $twig->render('templates/error.xml.twig', ['error' => "Error, callerid is missing"]);
+    echo $twig->render('error.xml.twig', [
+        'error' => "Error, callerid is missing"
+    ]);
     die();
 }
 else {
     $user_data = get_user($_GET['session_callerid']);
     if(!$user_data)
-        include "identification.php";
+        echo $twig->render('voice_record.xml.twig', [
+            'id'        => 'record_name',
+            'nextid'    => 'region',
+            'message'   => '<prompt>Welcome to the Seed System service.</prompt><prompt timeout="10s">Please identify yourself after the beep.</prompt>'
+        ]);
     else
         include "greet.php";
 }
