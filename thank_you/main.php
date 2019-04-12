42641b432875aeb3697aeafaef35871e975cadd1
Ersin <ersinpw@gmail.com>
2019-04-12 20:44:51 +0200

    Update main.php

--- thank_you/main.php
+++ thank_you/main.php
@@ -2,5 +2,5 @@
 echo $twig->render('message.xml.twig', [
     'id'        => 'thank_you',
     'message'   => '<break time="1000"/>Thank you',
-    'footer'    => '<submit next="identity/save_record.php" namelist="identity filename" method="post" enctype="multipart/form-data" fetchtimeout="10s"/>'
+    'footer'    => '<submit next="identity/save_record.php" namelist="record_name" method="post" enctype="multipart/form-data" fetchtimeout="10s"/>'
 ]);
