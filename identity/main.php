d62ec07b94659c0381f880f26ffb91f590b54d67
Ersin <ersinpw@gmail.com>
2019-04-12 17:45:48 +0200

    Update main.php

--- identity/main.php
+++ identity/main.php
@@ -13,7 +13,7 @@ else {
     if(!$user_data)
         echo $twig->render('voice_record.xml.twig', [
             'id'        => 'record_name',
-            'nextid'    => './identity/vxml/greet.vxml',
+            'nextid'    => '/identity/vxml/greet.vxml',
             'message'   => '<prompt>Welcome to the Seed System service.</prompt><prompt timeout="10s">Please identify yourself after the beep.</prompt>'
         ]);
     else
