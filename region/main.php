7eff053f48e3d9cdf0efe0d73f92d264f03f9084
Ersin <ersinpw@gmail.com>
2019-04-12 18:23:35 +0200

    Update main.php

--- region/main.php
+++ region/main.php
@@ -4,7 +4,7 @@ $region = file_get_contents("../json/region_mali.json");
 
 $json_decode = json_decode($region, true);
 
-echo $twig->render('voice_record.xml.twig', [
+echo $twig->render('choices.xml.twig', [
     'id'        => 'regions',
     'nextid'    => 'index.php?id=villages',
     'choices'   => array_keys($json_decode['region']),
