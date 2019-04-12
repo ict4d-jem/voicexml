325f92330b5432865fd91fd5b9af7ade536187c6
Ersin <ersinpw@gmail.com>
2019-04-12 21:17:41 +0200

    Update main.php

--- welcome/main.php
+++ welcome/main.php
@@ -3,5 +3,5 @@
 echo $twig->render('voice_record.xml.twig', [
     'id'        => 'record_name',
     'nextid'    => 'index.php?id=region&session_callerid='.$_GET['session_callerid'],
-    'message'   => '<prompt>Welcome to the Seed System service.</prompt><prompt timeout="10s">Please identify yourself after the beep.</prompt>'
+    'message'   => '<prompt>Welcome to the Seed System service.</prompt><prompt timeout="10s">Please identify yourself after the beep.<prompt timeout="1s"></prompt>'
 ]);
