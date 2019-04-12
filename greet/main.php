b9ea1f8c27824cc1fde62a8357626508cb5f0754
Ersin <ersinpw@gmail.com>
2019-04-12 22:52:49 +0200

    Update main.php

--- greet/main.php
+++ greet/main.php
@@ -2,7 +2,7 @@
 
 echo $twig->render('greet.xml.twig', [
     'id'        => 'greet',
-    'footer'    => '<goto expr="index.php?id=latest_crop&session_callerid='.$_GET['session_callerid'].'"/>',
+    'footer'    => '<goto expr="index.php?id=latest_crop&amp;session_callerid='.$_GET['session_callerid'].'"/>',
     'message'   => 'Hello',
     'message_end' => 'welcom to the crop seed program',
 ]);
