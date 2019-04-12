6ea046d3f73516ef097d2a532b068913e8aa84f5
Ersin <ersinpw@gmail.com>
2019-04-12 20:22:38 +0200

    Update main.php

--- identity/main.php
+++ identity/main.php
@@ -9,6 +9,8 @@ if(empty($_REQUEST['session_callerid'])) {
     die();
 }
 else {
+    $user_data = get_user($_GET['session_callerid']);
+    if(!$user_data)
         include "../welcome/main.php";
     else
         include "greet.php";
